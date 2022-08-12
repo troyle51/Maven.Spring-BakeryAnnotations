2d047218343b0413f613802011c10a3275abc004
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-08-12 15:38:22 -0400

    changed component to repository

--- src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
@@ -3,7 +3,8 @@ package com.zipcodewilmington.bakery.repositories;
 import com.zipcodewilmington.bakery.models.Muffin;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface MuffinRepository extends CrudRepository<Muffin, Long> {
 }
